cadbf68d73dfb916c7658499d93715c4e3726622
limanlin <435236478@qq.com>
2020-09-22 18:01:12 +0800

    构建eureka、item service、userservice、orderservice集群

--- sp03-userservice/src/main/java/cn/tedu/sp03/user/controller/UserController.java
+++ sp03-userservice/src/main/java/cn/tedu/sp03/user/controller/UserController.java
@@ -3,6 +3,7 @@ package cn.tedu.sp03.user.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.tedu.sp01.pojo.User;
@@ -27,7 +28,7 @@ public class UserController {
 
     @GetMapping("/{userId}/score")
     public JsonResult addScore(
-            @PathVariable Integer userId,  Integer score) {
+            @PathVariable Integer userId,  @RequestParam Integer score) {
         userService.addScore(userId, score);
         return JsonResult.ok();
     }
--- sp04-orderservice/src/main/java/cn/tedu/sp04/Sp04OrderserviceApplication.java
+++ sp04-orderservice/src/main/java/cn/tedu/sp04/Sp04OrderserviceApplication.java
@@ -2,8 +2,10 @@ package cn.tedu.sp04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Sp04OrderserviceApplication {
 
     public static void main(String[] args) {
