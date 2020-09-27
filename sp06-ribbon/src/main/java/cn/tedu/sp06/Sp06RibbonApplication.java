b2ea0c484bce8acddbd141e6bf27536766011170
limanlin <435236478@qq.com>
2020-09-27 17:28:27 +0800

    构建eureka、item service、userservice、orderservice集群

---
+++ sp06-ribbon/src/main/java/cn/tedu/sp06/Sp06RibbonApplication.java
@@ -0,0 +1,25 @@
+package cn.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp06RibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp06RibbonApplication.class, args);
+    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        SimpleClientHttpRequestFactory SCH = new SimpleClientHttpRequestFactory();
+        SCH.setConnectTimeout(1000);
+        SCH.setReadTimeout(1000);
+        return new RestTemplate();
+    }
+}
